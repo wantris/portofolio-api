09135144be4ebfb980952431b2c31668897e2289
wantris <gustiwacik@gmail.com>
2021-11-03 15:41:17 +0700

    fix cors again

--- app/Controllers/Api/Education.php
+++ app/Controllers/Api/Education.php
@@ -11,6 +11,13 @@ class Education extends ResourceController
 {
     use ResponseTrait;
 
+    public function __construct()
+    {
+        Header('Access-Control-Allow-Origin: *'); //for allow any domain, insecure
+        Header('Access-Control-Allow-Headers: *'); //for allow any headers, insecure
+        Header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE');
+    }
+
     public function index()
     {
         $model = new EducationModel();
--- app/Controllers/Api/Skill.php
+++ app/Controllers/Api/Skill.php
@@ -10,6 +10,14 @@ use CodeIgniter\API\ResponseTrait;
 class Skill extends ResourceController
 {
     use ResponseTrait;
+
+    public function __construct()
+    {
+        Header('Access-Control-Allow-Origin: *'); //for allow any domain, insecure
+        Header('Access-Control-Allow-Headers: *'); //for allow any headers, insecure
+        Header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE');
+    }
+
     public function index()
     {
         try {
